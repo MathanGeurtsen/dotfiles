097776e6a96d9e4899b30baa95b1a674374f99bf
Mathan Geurtsen <me@mathangeurtsen.nl>
2021-05-01 13:58:22 +0200

    added simple deploy script
    This disploy script simply symlinks relevant dotfiles. One might argue
      that this could be better automated, but this seems to me like premature
      optimization at this point: this script concerns but four
      symlinks.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+echo $SCRIPT_DIR
+ln -s "$SCRIPT_DIR/.bashrc" ~/.bashrc
+ln -s "$SCRIPT_DIR/.zshrc" ~/.zshrc
+ln -s "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
+ln -s "$SCRIPT_DIR/.emacs.d" ~/.emacs.d
