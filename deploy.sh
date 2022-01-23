b5c113cd6f3f9270ef54641ed18118470d7f1653
MathanGeurtsen <me@mathangeurtsen.nl>
2022-01-23 10:18:51 +0100

    Rprofile added
    an attempt to make history saving more reasonable
    (I haven't quite succeeded)

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ ln -s "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
 ln -s "$DOTFILES_DIR/.emacs.d" ~/.emacs.d
 ln -s "$DOTFILES_DIR/.xprofile" ~/.xprofile
 ln -s "$DOTFILES_DIR/.vimrc" ~/.vimrc
+ln -s "$DOTFILES_DIR/.Rprofile" ~/.Rprofile
